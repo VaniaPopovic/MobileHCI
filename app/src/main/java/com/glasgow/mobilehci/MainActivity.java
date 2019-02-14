a8279cecebe2eeaf59475c39bf1fc16a699f5012
odissea.15 <odisseasj18@gmail.com>
2019-02-14 11:57:50 +0000

    first test

--- app/src/main/java/com/glasgow/mobilehci/MainActivity.java
+++ app/src/main/java/com/glasgow/mobilehci/MainActivity.java
@@ -2,12 +2,31 @@ package com.glasgow.mobilehci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    boolean hi = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       final Button button = findViewById(R.id.mybutton);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                if(hi)button.setText("HELLO");
+                else button.setText("hasan");
+                hi=!hi;
+            }
+        });
     }
 }
